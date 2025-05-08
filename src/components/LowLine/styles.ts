9a674c046fd712608177fe67db281c173f6b0dfc
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-05-07 23:54:40 -0300

    feat: created LowLine component

---
+++ src/components/LowLine/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components/native";
+
+export type LowLineTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+
+type Props = {
+    type: LowLineTypeStyleProps;
+    login?: boolean;
+}
+
+export const Container = styled.View`
+    padding: 12px;
+
+    align-items: center;
+`;
+
+export const TextBase = styled.Text `
+    font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+`;
+
+export const LinkText = styled.Text<Props>`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+
+  color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.AQUA : theme.COLORS.GREEN_400};
+`;
+
