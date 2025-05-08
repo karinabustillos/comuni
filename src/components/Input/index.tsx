ad4c71ff579a8c40ec48ef0f6d9daacc9d0f129f
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-05-08 11:13:50 -0300

    feat: enhance Input component with Unform integration and new props typin

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,29 +1,40 @@
-import React, { 
-  forwardRef,
-  useRef,
-  useState,
-  useEffect,
-  useCallback,
-  useImperativeHandle,
-} from 'react';
-
-import { TextInputProps } from 'react-native';
-
+import React, { useEffect, useRef } from 'react';
+import { TextInput, TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 import { Container, InputContent, Title } from './styles';
 
-type InputProps = TextInputProps & {
-    text?: string;
+type Props = TextInputProps & {
+  name: string;
+  text?: string;
 };
 
-export default function Input({ text, ...rest }: InputProps) {
-    return (
-        <Container>
-            <Title>
-                {text}
-            </Title>
-            <InputContent
-                {...rest}
-            />
-        </Container>
-    );
+export default function Input({ name, text, ...rest }: Props) {
+  const inputRef = useRef<TextInput>(null);
+
+  const { fieldName, registerField, defaultValue = '', error } = useField(name);
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputRef.current,
+      getValue: ref => ref?.value || '',
+      setValue: (ref, value) => {
+        if (ref) ref.setNativeProps({ text: value });
+      },
+      clearValue: ref => ref?.clear(),
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      <Title>{text}</Title>
+      <InputContent
+        ref={inputRef}
+        defaultValue={defaultValue}
+        placeholderTextColor="#aaa"
+        {...rest}
+      />
+      {error && <Title style={{ color: 'red' }}>{error}</Title>}
+    </Container>
+  );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,14 +2,14 @@ import styled from "styled-components/native";
 import { TextInput } from 'react-native';
 
 export const Container = styled.View`
-    margin-bottom: 25px;
+    margin-bottom: 5px;
 `;
 
 export const Title = styled.Text`
         font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
         font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
 
-        margin: 0 20px 20px;
+        margin: 0 20px;
         color: ${({ theme }) => theme.COLORS.WHITE};
 `;
 
