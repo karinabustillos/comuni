e7f0b25b58ce0280ca037a5a9c70cd5d42ecaa2a
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-05-07 23:56:28 -0300

    chore:  update import

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { Container, Logo } from './styles';
 
 import logoImg from '@assets/pre-logo.png';
 
-export function Groups() {
+export default function Groups() {
   return (
     <Container>
       <Logo source={logoImg} />
