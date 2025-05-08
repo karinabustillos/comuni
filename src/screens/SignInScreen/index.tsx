582ac29cda533644c71874260fa20064e1a7b2a6
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-05-08 01:16:44 -0300

    refactor(component): rename NewGroups to SignInScreen

---
+++ src/screens/SignInScreen/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Container } from './styles';
+
+import Header from '@components/Header'
+import HighLine from '@components/HighLine'
+import LowLine from '@components/LowLine'
+import Input from "@components/Input";
+import Buttun from '@components/Button'
+
+
+export default function SignInScreen() {
+  return (
+    <Container>
+      <Header text="Sair" />
+      <HighLine text="Crie uma nova conta" />
+      <LowLine
+        type="PRIMARY"
+        text="Já é cadastrado? Faça "
+        link="login" />
+      <Input
+        text="Nome"
+        placeholder="Digite seu nome completo"
+      />
+      <Input
+        text="Senha"
+        placeholder="********"
+      />
+      <LowLine
+        type="SECONDARY"
+        link="Esqueci minha senha"
+        />
+      <Buttun 
+        title="Sign In"
+      />
+    </Container>
+  );
+}
