ef829d2b9aef7eabd593f5ac1eb3e206d2d43fb6
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-06-09 18:26:14 -0300

    chore(types): update AppStackParamList and ActivityStackParamList with new routes

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,25 +1,24 @@
 export type AppStackParamList = {
-   // Onboarding: undefined;
-    //Cadastro: undefined;
-    //Login: undefined;
-    LogInScreen: undefined; 
-    ResetPasswordScreen: undefined;
-    SignInScreen: undefined;
-    Main: undefined;
-  };
-  
-  export type MainTabParamList = {
-    Home: undefined;
-    Explore: undefined;
-    Atividades: undefined;
-    Configurações: undefined;
-  };
-
-  export type ActivityStackParamList = {
-    ActivityScreen: undefined;
-    CreateActivityScreen: undefined;
+  LogInScreen: undefined;
+  ResetPasswordScreen: undefined;
+  SignInScreen: undefined;
+  YourEventsScreen: undefined;
+  EventDetail: { eventId: string };
+  Main: undefined;
 };
-  
- // export type RootStackParamList = AppStackParamList & MainTabParamList;
 
- 
+export type MainTabParamList = {
+  Home: undefined;
+  Explore: undefined;
+  Atividades: undefined;
+  Configurações: undefined;
+};
+
+export type ActivityStackParamList = {
+  ActivityScreen: undefined;
+  CreateActivityScreen: undefined;
+  YourEventsScreen: undefined;
+  EventDetail: { eventId: string };
+};
+
+
